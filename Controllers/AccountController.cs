4ba507a20294f04fa658f672698128327890b0fb
Dnyanada-deshkar <dnyanadadeshkar@gmail.com>
2026-07-01 15:57:41 +0530

    login page UI

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using StationeryInventorySystem.Models;
+
+namespace StationeryInventorySystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Username == "admin" &&
+                    model.Password == "admin123")
+                {
+                    return RedirectToAction("Index", "Dashboard");
+                }
+
+                ViewBag.Error = "Invalid Username or Password";
+            }
+
+            return View(model);
+        }
+    }
+}
